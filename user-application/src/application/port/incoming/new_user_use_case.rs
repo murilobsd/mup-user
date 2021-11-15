4e673707260d5eebedc77e018cbc6c8656bfc47d
msi <msbd@m0x.ru>
2021-11-15 13:06:03 -0300

    fix: Change fields of user based on sql

--- user-application/src/application/port/incoming/new_user_use_case.rs
+++ user-application/src/application/port/incoming/new_user_use_case.rs
@@ -7,25 +7,11 @@ pub struct NewUserCommand {
     pub email: String,
     /// The hashed password of user
     pub password: String,
-    /// The first name of user
-    pub first_name: String,
-    /// The last name of user
-    pub last_name: String,
 }
 
 impl NewUserCommand {
-    pub fn new_without_id(
-        email: String,
-        password: String,
-        first_name: String,
-        last_name: String,
-    ) -> Self {
-        Self {
-            email,
-            password,
-            first_name,
-            last_name,
-        }
+    pub fn new_without_id(email: String, password: String) -> Self {
+        Self { email, password }
     }
 }
 
--- user-application/src/domain/user.rs
+++ user-application/src/domain/user.rs
@@ -10,36 +10,37 @@ pub struct User {
     /// the email of user
     pub email: String,
     /// the hashed password of user
+    pub username: Option<String>,
+    /// the hashed password of user
     pub password: String,
+    /// the salt password of user
+    pub salt: String,
     /// the first name of user
-    pub first_name: String,
-    /// the last name of user
-    pub last_name: String,
-    /// the created date of user
     pub created_at: DateTime<Utc>,
     /// the last date of updated info about
     pub updated_at: Option<DateTime<Utc>>,
-    /// the user as admin
-    pub is_admin: bool,
+    /// the last date of updated info about
+    pub confirmed_at: Option<DateTime<Utc>>,
+    /// the user id active
+    pub active: bool,
 }
 
 impl User {
     pub fn new_without_id(
         email: String,
+        salt: String,
         password: String,
-        first_name: String,
-        last_name: String,
-        is_admin: bool,
     ) -> Self {
         Self {
             id: None,
             email,
+            username: None,
+            salt,
             password,
-            first_name,
-            last_name,
-            is_admin,
+            active: false,
             created_at: Utc::now(),
             updated_at: None,
+            confirmed_at: None,
         }
     }
 }
